75e4cd289b795477f02ea33751a6c4667f68115c
Shun Nakano <nakano_syun@waku-2.com>
2020-09-16 11:53:28 +0900

    modules/todoの作成

--- src/client/redux/modules/reducers.ts
+++ src/client/redux/modules/reducers.ts
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import todo from './todo';
 import todos from './todos';
 import project from './project';
 import projects from './projects';
@@ -7,6 +8,7 @@ import todoFormIsShown from './todoFormIsShown';
 
 // 複数のreducerを一つにまとめる
 const rootReducer = combineReducers({
+	todo,
 	todos,
 	project,
 	projects,
---
+++ src/client/redux/modules/todo.ts
@@ -0,0 +1,43 @@
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { createSelector } from 'reselect';
+import { RootState } from './reducers';
+import { getToday } from '../../libs/utilFunctions';
+import { Todo } from './todos';
+
+const actionCreator = actionCreatorFactory();
+
+type ChangeTodo = {
+	key: string;
+	value: string | number;
+};
+
+export const changeTodo = actionCreator<ChangeTodo>('CHANGE_TODO');
+export const resetTodo = actionCreator('RESET_TODO');
+export const editTodo = actionCreator<Todo>('EDIT_TODO');
+
+const initialState: Todo = {
+	id: 0,
+	title: '',
+	dueDate: getToday(),
+	status: 'new',
+	projectId: 0,
+};
+
+const reducer = reducerWithInitialState(initialState)
+	.case(changeTodo, (state, payload) => {
+		return { ...state, [payload.key]: payload.value };
+	})
+	.case(resetTodo, () => {
+		return { ...initialState };
+	})
+	.case(editTodo, (state, payload) => {
+		return { ...state, ...payload };
+	});
+
+export default reducer;
+
+export const selectTodo = createSelector(
+	[(state: RootState) => state.todo],
+	(todo) => todo,
+);
