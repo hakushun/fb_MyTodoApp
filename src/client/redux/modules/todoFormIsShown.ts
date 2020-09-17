fc35275334f9589656bb3e84e6f9174cd13dba4e
Shun Nakano <nakano_syun@waku-2.com>
2020-09-17 14:33:25 +0900

    modules/todoFormIsShownにcase文追加

--- src/client/redux/modules/todoFormIsShown.ts
+++ src/client/redux/modules/todoFormIsShown.ts
@@ -2,6 +2,9 @@ import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { createSelector } from 'reselect';
 import { RootState } from './reducers';
+import { toggleAriaHidden, toggleScrollLock } from '../../libs/utilFunctions';
+import { addTodo, updateTodo } from './todos';
+import { editTodo, resetTodo } from './todo';
 
 const actionCreator = actionCreatorFactory();
 
@@ -9,12 +12,32 @@ export const toggleTodoFormIsShown = actionCreator<boolean>(
 	'toogle_todoFormIsShown',
 );
 
-const reducer = reducerWithInitialState(false).case(
-	toggleTodoFormIsShown,
-	(_state, payload) => {
+const reducer = reducerWithInitialState(false)
+	.case(toggleTodoFormIsShown, (_state, payload) => {
+		toggleAriaHidden(payload);
+		toggleScrollLock(payload);
 		return payload;
-	},
-);
+	})
+	.case(addTodo, () => {
+		toggleAriaHidden(false);
+		toggleScrollLock(false);
+		return false;
+	})
+	.case(editTodo, () => {
+		toggleAriaHidden(true);
+		toggleScrollLock(true);
+		return true;
+	})
+	.case(updateTodo, () => {
+		toggleAriaHidden(false);
+		toggleScrollLock(false);
+		return false;
+	})
+	.case(resetTodo, () => {
+		toggleAriaHidden(false);
+		toggleScrollLock(false);
+		return false;
+	});
 
 export default reducer;
 
